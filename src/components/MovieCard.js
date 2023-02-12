0eec0c7a003958bf0df9ef29d73ddeefc0c367f3
fmitei <faith.mitei@trustgro.com>
2023-02-12 09:46:19 +0300

    testing nested destructuring

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,10 +4,11 @@ function MovieCard({
   title,
   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
   genres,
+  poster,
 }) {
   return (
     <div className="movie-card">
-      <img src={posterSrc} alt={title} />
+      <img src={poster} alt={title} />
       <h2>{title}</h2>
       <small>{genres.join(", ")}</small>
     </div>
